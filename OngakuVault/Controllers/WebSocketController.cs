a92d92e2dbecfd2a7380b28e5f5c4aa3a6de9734
Med <45147847+kitsumed@users.noreply.github.com>
2024-12-22 00:59:40 -0500

    Add websocket-support

---
+++ OngakuVault/Controllers/WebSocketController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Components.RenderTree;
+using Microsoft.AspNetCore.Mvc;
+using OngakuVault.Services;
+using System.Net.WebSockets;
+
+namespace OngakuVault.Controllers
+{
+	/// <summary>
+	/// Handle the connection of clients to the WebSocketService
+	/// </summary>
+	[ApiController]
+	public class WebSocketController : ControllerBase
+	{
+		private ILogger<WebSocketController> _logger;
+		private IWebSocketManagerService _webSocketManagerService;
+
+		public WebSocketController(ILogger<WebSocketController> logger, IWebSocketManagerService webSocketManagerService)
+		{
+			_logger = logger;
+			_webSocketManagerService = webSocketManagerService;
+		}
+
+		[HttpGet("test")]
+		[EndpointDescription("test")]
+		[Produces("application/json")]
+		public ActionResult test()
+		{
+			_webSocketManagerService.BroadcastAsync("test message!");
+			return Ok();
+		}
+
+		[Route("/ws")] // See https://learn.microsoft.com/en-us/aspnet/core/fundamentals/websockets?view=aspnetcore-8.0
+		[ApiExplorerSettings(IgnoreApi = true)] // Swagger does not support websocket (as it require a mvc.HttpGetAttribute, who cause websocket to stop working)
+		public async Task ConnectToWebSocket()
+		{
+			// Ensure the request is a websocket
+			if (!HttpContext.WebSockets.IsWebSocketRequest)
+			{
+				HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+				return;
+			}
+
+			using WebSocket webSocketConnection = await HttpContext.WebSockets.AcceptWebSocketAsync();
+			// Add the websocket connection to the websocket manager list
+			bool wasWebSocketClientAdded = _webSocketManagerService.TryAddClient(webSocketConnection, out Guid webSocketClientId);
+			if (!wasWebSocketClientAdded)
+			{
+				_logger.LogWarning("Failed to add websocket connection to the WebSocketManagerService list.");
+				HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+				return;
+			};
+
+			try
+			{
+				// Keep the WebSocket open until the client disconnect
+				byte[] buffer = new byte[1024 * 4]; // 4kb buffer
+				while (webSocketConnection.State == WebSocketState.Open)
+				{
+					WebSocketReceiveResult websocketResult = await webSocketConnection.ReceiveAsync(buffer, CancellationToken.None);
+					// Check if websocket connection is closing
+					if (websocketResult.CloseStatus.HasValue)
+					{
+						// Send a close handshake to the client to handle closure
+						await webSocketConnection.CloseAsync(websocketResult.CloseStatus.Value, websocketResult.CloseStatusDescription, CancellationToken.None);
+						break;
+					}
+
+					// We don't do anything the the client received data (websocketResult)
+				}
+			}
+			catch (WebSocketException ex) 
+			{
+				// Do not log if the error is related to Prematurely closed websocket (Can happen at server closure with clients connected for example)
+				if (ex.WebSocketErrorCode != WebSocketError.ConnectionClosedPrematurely) 
+				{
+					_logger.LogError(ex, "Unexpected WebSocket error occured with connection client ID : '{clientId}'. Error: {message}", webSocketClientId, ex.Message);
+				}
+			}
+			finally
+			{
+				// Remove and dispose of the client connection
+				_ = _webSocketManagerService.TryRemoveClient(webSocketClientId);
+			}
+		}
+	}
+}
---
+++ OngakuVault/Services/WebSocketManagerService.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace OngakuVault.Services
+{
+	public interface IWebSocketManagerService
+	{
+		/// <summary>
+		/// Try to add a websocket connection to the connections list of the WebSockerManager.
+		/// </summary>
+		/// <param name="webSocket">The websocket connection</param>
+		/// <param name="clientId">The unique clientId created for that websocket connection</param>
+		/// <returns>True if it was added, false if it failed</returns>
+		public bool TryAddClient(WebSocket webSocket, out Guid clientId);
+
+		/// <summary>
+		/// Try to remove and dispose of the websocket connection of a specific client inside the
+		/// WebSocketManager list.
+		/// </summary>
+		/// <param name="clientId">The unique clientId of the websocket connection</param>
+		/// <returns>True if it was removed and disposed, else false</returns>
+		public bool TryRemoveClient(Guid clientId);
+
+		public Task BroadcastAsync(string message);
+	}
+
+	/// <summary>
+	/// This class implement the <see cref="IWebSocketManagerService"></see> interface,
+	/// allowing to manages websocket connections
+	/// </summary>
+	public class WebSocketManagerService : IWebSocketManagerService
+	{
+		/// <summary>
+		/// List of clients connection to the websocket
+		/// </summary>
+		private readonly ConcurrentDictionary<Guid, WebSocket> ClientsConnection = new ConcurrentDictionary<Guid, WebSocket>();
+
+		public bool TryAddClient(WebSocket webSocket, out Guid clientId)
+		{
+			clientId = Guid.NewGuid();
+			return ClientsConnection.TryAdd(clientId, webSocket);
+		}
+
+		public bool TryRemoveClient(Guid clientId)
+		{
+			bool sucess = ClientsConnection.TryRemove(clientId, out WebSocket? webSocket);
+			if (sucess)
+			{
+				webSocket?.Dispose();
+			}
+			return sucess;
+		}
+
+		public async Task BroadcastAsync(string message)
+		{
+			// Convert the message to UTF8 bytes
+			byte[] buffer = Encoding.UTF8.GetBytes(message);
+			// Run multiple async thread for every client connection
+			IEnumerable<Task> allWebSocketTaks = ClientsConnection.Values.Select(async webSocket =>
+			{
+				if (webSocket.State == WebSocketState.Open)
+				{
+					await webSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+				}
+			});
+			await Task.WhenAll(allWebSocketTaks);
+		}
+	}
+}
