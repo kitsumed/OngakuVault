b2b1755d1a44d34b50000a72d26accc97c326aa0
Med <45147847+kitsumed@users.noreply.github.com>
2025-03-27 23:30:08 -0400

    fix(cli): Rewrote prepare for release ps1
    Now parsing artifacts as directory insead of zip files.

--- .github/workflows/scripts/prepare_github_build_artifacts_for_release.ps1
+++ .github/workflows/scripts/prepare_github_build_artifacts_for_release.ps1
@@ -6,8 +6,8 @@ param (
     [switch]$compressOutput
 )
 
-# Check if the artifactDir exists
-if (-not (Test-Path -Path $artifactDir)) {
+# Check if the artifactDir does not exists
+if (!(Test-Path -Path $artifactDir)) {
     Write-Warning "The artifact directory '$artifactDir' does not exist."
     exit 1
 }
@@ -16,75 +16,67 @@ if (-not (Test-Path -Path $artifactDir)) {
 New-Item -Path $outputDir -ItemType Directory -ErrorAction SilentlyContinue
 
 # Iterate over the artifacts and process them
-$artifacts = Get-ChildItem -Path $artifactDir -File
-foreach ($artifactFile in $artifacts) {
-    Write-Host "Processing artifact: $artifactFile"
+$artifacts = Get-ChildItem -Path $artifactDir -Directory
+foreach ($artifactFolder in $artifacts) {
+    Write-Host "Processing artifact: $artifactFolder"
 
-    # If the artifact is a ZIP archive (should always be)
-    if ($artifactFile.Extension -eq ".zip") {
-        # If the artifact name starts with "win-", do not extract, just calculate the hash
-        if ($artifactFile.Name -like "win-*") {
-            Write-Host "Detected $artifactFile as a Windows build."
-            # If compress output flag is enabled, keep current zip archive, else unzip
-            if ($compressOutput) {
-                # Move win build to output directory
-                Move-Item -Path $artifactFile -Destination $outputDir
-            } else {
-                # Extract zip artifact to the outpoutDir
-                $extractDir = "$outputDir\$($artifactFile.BaseName)"
-                Write-Host "Extracting $artifactFile to $extractDir"
-                Expand-Archive -Path $artifactFile.FullName -DestinationPath $extractDir
-                # Remove the zip artifact archive
-                Write-Host "Cleaning up zip artifact archive : $artifactFile"
-                Remove-Item -Path "$artifactFile" -Recurse -ErrorAction SilentlyContinue
+
+    # If the artifact name starts with "win-", do not extract, just calculate the hash
+    if ($artifactFolder.Name -like "win-*") {
+        Write-Host "Detected $artifactFolder as a Windows build."
+        # If compress output flag is enabled, compress to output, else move directory to output
+        if ($compressOutput) {
+            # Compress files to output
+            Write-Host "Compressing $artifactFolder to $zipFilePath"
+            $zipFilePath = "$outputDir\$($artifactFolder.Name).zip"
+            Compress-Archive -Path "$($artifactFolder.FullName)\*" -DestinationPath $zipFilePath
+            Write-Host "Cleaning up artifact archive : $artifactFolder"
+            Remove-Item -Path "$artifactFolder" -Recurse -ErrorAction SilentlyContinue
+        }
+        else {
+            Move-Item -Path $artifactFolder.FullName -Destination $outputDir
+        }
+    }
+    # If the artifact name starts with "linux-", extract, apply chmod, recompress (tar.gz), and calculate hash
+    elseif ($artifactFolder.Name -like "linux-*") {
+        Write-Host "Detected $artifactFolder as a Linux build."
+        if ($IsLinux) {
+            # The name of the binaries to apply chmod to
+            $binariesToChmod = @("yt-dlp", "ffmpeg", "ffprobe")
+            foreach ($binaryName in $binariesToChmod) {
+                $binaryPath = Join-Path -Path $artifactFolder.FullName -ChildPath $binaryName
+                if (Test-Path -Path $binaryPath) {
+                    Write-Host "Adding read and execution permission to $binaryName"
+                    chmod a+rx $binaryPath
+                }
+                else {
+                    Write-Warning "Could not chmod $binaryName, binary not found."
+                }
             }
         }
-        # If the artifact name starts with "linux-", extract, apply chmod, recompress (tar.gz), and calculate hash
-        elseif ($artifactFile.Name -like "linux-*") {
-            Write-Host "Detected $artifactFile as a Linux build."
-            # Extract zip artifact
-            $extractDir = "$artifactDir\$($artifactFile.BaseName)"
-            Write-Host "Extracting $artifactFile to $extractDir"
-            Expand-Archive -Path $artifactFile.FullName -DestinationPath $extractDir
-
-            if ($IsLinux) {
-                # The name of the binaries to apply chmod to
-                $binariesToChmod = @("yt-dlp", "ffmpeg", "ffprobe")
-                foreach ($binaryName in $binariesToChmod) {
-                    $binaryPath = Join-Path -Path $extractDir -ChildPath $binaryName
-                    if (Test-Path -Path $binaryPath) {
-                        Write-Host "Adding read and execution permission to $binaryName"
-                        chmod a+rx $binaryPath
-                    } else {
-                        Write-Warning "Could not chmod $binaryName, binary not found."
-                    }
-                }
-            } else {
-                Write-Warning("Could not apply chmod to binary as the script is running on a non-linux system.")
-            }
+        else {
+            Write-Warning("Could not apply chmod to binary as the script is running on a non-linux system.")
+        }
 
 
-            if ($compressOutput) {
-                # Recompress the directory into a tar.gz archive in the outputDir (preserving chmod)
-                $tarFilePath = "$outputDir\$($artifactFile.BaseName).tar.gz"
-                Write-Host "Recompressing the extracted files into $tarFilePath"
-                # Create a tar.gz archive
-                tar -czf "$tarFilePath" -C "$extractDir" *
-                # Check if the command failed (prevent deleting original files from $artifactDir)
-                if (!$?) {
-                    Write-Warning "An error occurred while creating the tar.gz archive. Stopping execution..."
-                    exit 1
-                }
-                # Remove the zip artifact archive and it's decompression
-                Write-Host "Cleaning up zip artifact archive : $artifactFile"
-                Remove-Item -Path "$artifactFile" -Recurse -ErrorAction SilentlyContinue
-                Write-Host "Cleaning up decompressed archive : $extractDir"
-                Remove-Item -Path "$extractDir" -Recurse -ErrorAction SilentlyContinue
+        if ($compressOutput) {
+            # Recompress the directory into a tar.gz archive in the outputDir (preserving chmod)
+            $tarFilePath = "$outputDir\$($artifactFolder.BaseName).tar.gz"
+            Write-Host "Recompressing the extracted files into $tarFilePath"
+            # Create a tar.gz archive
+            tar -czf "$tarFilePath" -C "$artifactFolder" *
+            # Check if the command failed (prevent deleting original files from $artifactDir)
+            if (!$?) {
+                Write-Warning "An error occurred while creating the tar.gz archive. Stopping execution..."
+                exit 1
             }
-            else {
-                # Move the extracted folder to the output directory
-                Move-Item -Path $extractDir -Destination $outputDir
-            }
+            # Remove the zip artifact archive and it's decompression
+            Write-Host "Cleaning up artifact archive : $artifactFolder"
+            Remove-Item -Path "$artifactFolder" -Recurse -ErrorAction SilentlyContinue
+        }
+        else {
+            # Move the extracted folder to the output directory
+            Move-Item -Path $artifactFolder.FullName -Destination $outputDir
         }
     }
 }
